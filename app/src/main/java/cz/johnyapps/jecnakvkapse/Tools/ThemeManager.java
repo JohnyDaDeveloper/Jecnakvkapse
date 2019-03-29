a7d99ca6083ae82b72374c6e1c33648d676e7ae2
JohnyDaDeveloper <johnydadeveloper@gmail.com>
2019-03-29 17:38:25 +0100

    Napsání javadoc pro ThemeManager

--- app/src/main/java/cz/johnyapps/jecnakvkapse/Tools/ThemeManager.java
+++ app/src/main/java/cz/johnyapps/jecnakvkapse/Tools/ThemeManager.java
@@ -7,19 +7,28 @@ import cz.johnyapps.jecnakvkapse.R;
 
 import static android.content.Context.MODE_PRIVATE;
 
+/**
+ * Stará se o načítání témat (themes). <b>Je potřeba přidat do onCreate KAŽDÉ AKTIVITY před setContentView</b>.
+ */
 public class ThemeManager {
     private Context context;
     private SharedPreferences prefs;
 
+    /**
+     * Inicializace
+     * @param context   Context
+     */
     public ThemeManager(Context context) {
         this.context = context;
+
         prefs = context.getSharedPreferences("jecnakvkapse", MODE_PRIVATE);
     }
 
+    /**
+     *  Načte nastavené téma z paměti zařízení
+     */
     public void loadTheme() {
         int theme = prefs.getInt("selected_theme", R.id.SettingsTheme_light);
-
-
 
         switch (theme) {
             case R.id.SettingsTheme_dark: {
