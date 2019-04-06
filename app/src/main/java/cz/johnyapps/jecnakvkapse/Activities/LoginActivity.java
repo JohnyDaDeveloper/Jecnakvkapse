2732dd51eb14de00ec65d9adc01c40aa1dafae8f
JohnyDaDeveloper <johnydadeveloper@gmail.com>
2019-04-06 23:02:49 +0200

    Vytvoření login aktivity

---
+++ app/src/main/java/cz/johnyapps/jecnakvkapse/Activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package cz.johnyapps.jecnakvkapse.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.johnyapps.jecnakvkapse.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
