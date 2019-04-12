df47bc3095813f8e9ec0179c46075cdf82566626
JohnyDaDeveloper <johnydadeveloper@gmail.com>
2019-04-12 23:25:16 +0200

    Dokumentace pro SplashScreen

--- app/src/main/java/cz/johnyapps/jecnakvkapse/Activities/SplashActivity.java
+++ app/src/main/java/cz/johnyapps/jecnakvkapse/Activities/SplashActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Aktivita která zobrazí iconu na tmavém pozadí během načítání {@link MainActivity}
+ */
 public class SplashActivity extends AppCompatActivity {
 
     @Override
